351d16217418f257e0cbb71904c4ea6dad8f6849
Dominic Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2024-09-18 09:59:17 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,23 +8,24 @@ sudo apt upgrade -y
 
 # common programs
 sudo apt remove w3m -y
+sudo apt install git -y
 sudo apt install gh -y
 sudo apt install stow -y
+sudo apt install tree -y
 
 # install NeoVim from source
 mkdir temp
-cd temp
-sudo apt-get install ninja-build gettext cmake unzip curl
+cd temp || exit
+sudo apt-get install ninja-build gettext cmake unzip curl -y
 git clone https://github.com/neovim/neovim
 cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
-cd ..
-cd ..
+cd .. || exit
+cd .. || exit
 sudo rm -r ./temp
 
 # install languages
-curl -fsSL https://bun.sh/install | bash
 sudo apt install -y default-jdk
+curl -fsSL https://bun.sh/install | bash
 
-# copy over the scripts folder into the home directory
-cp -r ./scripts/ ~/
+source ~/.bashrc
