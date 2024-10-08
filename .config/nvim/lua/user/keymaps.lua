1bccaa32a4547a46feb2790dc710c29ca9ddfb5d
Dominic Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2024-10-08 09:50:35 -0400

    fixed keymaps

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -4,10 +4,10 @@ vim.g.mapleader=" "
 
 -- Normal (Command Mode) Mode --
 -- Better window navigation --
--- vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
--- vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
--- vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
--- vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
+vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
+vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
+vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
+vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
 -- open up file explorer on the left hand side --
 vim.keymap.set("n", "<leader>e", ":Lex 30<CR>", opts)
 
