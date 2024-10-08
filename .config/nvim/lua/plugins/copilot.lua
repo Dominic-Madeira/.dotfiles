5b34304055e5d9599ffe92cabd8c651ef830d927
Dominic Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2024-10-08 09:38:16 -0400

    changed to codeium

--- .config/nvim/lua/plugins/copilot.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "github/copilot.vim",
-  config = function()
-    vim.g.copilot_no_tab_map = true
-    vim.api.nvim_set_keymap("i", "<C-c>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-  end
-}
