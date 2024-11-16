7585036abae44f827ae5d3ea4d555f1aee437767
Dominic Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2024-11-15 19:20:26 -0500

    added some commands to assist with the hotkeys

---
+++ imLazyScripts/javalint.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+java -jar ~/.dotfiles/imLazyScripts/linter/checkstyle.app -c ~/.dotfiles/imLazyScripts/linter/sun_checks.xml *.java
---
+++ imLazyScripts/push.sh
@@ -0,0 +1,9 @@
+#!/user/bin/env bash
+# Created by: Dominic M.
+# Created on: Oct: 2024
+# git push script
+
+git add -A
+# sorry in advance for a bad commit message
+git commit -m 'pushed changes'
+git push origin main
